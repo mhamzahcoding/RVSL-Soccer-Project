d3ed6982b6da0405b2a615a6e2383fa1b308fbdc
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-15 16:19:58 -0500

    back end completed and front end layout done

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://Hamzah:Maxima1@ds137090.mlab.com:37090/finalproject-db',
+  secretOrKey: 'secret'
+};
---
+++ validation/is-empty.js
@@ -0,0 +1,10 @@
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
+
+// this is the special isEmpty function that check for undefined and null value
+// also it checks for an empty string and empty object
