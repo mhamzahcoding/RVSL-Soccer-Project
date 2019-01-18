3df6a79fe72596ee596c09f84a8914e3459da5fa
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-17 22:09:31 -0500

    edit profile working

---
+++ client/src/components/dashboard/ProfileActions.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProfileActions = () => {
+  return (
+    <div>
+      <div class="btn-group mb-4" role="group">
+        <Link to="/edit-profile" class="btn btn-light">
+          <i class="fas fa-user-circle text-info mr-1" /> Edit Profile
+        </Link>
+        <Link to="/add-experience" class="btn btn-light">
+          <i class="fab fa-black-tie text-info mr-1" />
+          Add Experience
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileActions;
