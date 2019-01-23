7f28d67559d336a7470f46b4579bf2ec452f8e61
Nico Almena <fnalmenaradumur96@gmail.com>
2019-01-22 19:24:34 -0500

    table

---
+++ client/src/components/leagueTable/Columns.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import league from '../../img/league';
+import './rows.css';
+import {Link} from 'react-router-dom';
+
+
+function rowClassNameFormat(row, rowIdx) {
+    // row is whole row object
+    // rowIdx is index of row
+    return rowIdx  < 4 ? 'td-column-function-even-example' : 'td-column-function-odd-example';
+
+
+  }
+
+class Table extends React.Component {
+    
+    
+
+
+    render() {
+        console.log(league.league)
+        const teams = league.league
+          
+
+      return (
+        <BootstrapTable 
+        data={ teams } 
+        hover
+        condensed 
+        trClassName= {rowClassNameFormat}
+        >
+          <TableHeaderColumn dataField='id'isKey>id</TableHeaderColumn>
+          <Link to="/profiles">
+          <TableHeaderColumn dataField='name'> Team Name  </TableHeaderColumn>
+          </Link>
+          <TableHeaderColumn dataField='position'>position</TableHeaderColumn>
+          <TableHeaderColumn dataField='wins'>Wins</TableHeaderColumn>
+          <TableHeaderColumn dataField='ties'>Ties</TableHeaderColumn>
+          <TableHeaderColumn dataField='losses'>Losses</TableHeaderColumn>
+          <TableHeaderColumn dataField='points'>points</TableHeaderColumn>
+        </BootstrapTable>
+      );
+    }
+  }
+
+export default Table
--- client/src/components/leagueTable/LeagueTable.js
+++ client/src/components/leagueTable/LeagueTable.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import ReactTable from 'react-table'
+//import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import { Jumbotron } from 'react-bootstrap'
+import Table from './Columns';
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import ErrorBoundary from '../../components/errorboundaries';
+
 //import { Card } from 'react-bootstrap'
 ///import {Container}  from 'react-bootstrap'
 //import axios from 'axios';
@@ -9,67 +13,16 @@ import { Jumbotron } from 'react-bootstrap'
 
 
 class LeagueTable extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
+      render() {  
+
+        return (
+            <ErrorBoundary>
+            <Jumbotron>
+                <Table/>
+            </Jumbotron>
+        </ErrorBoundary>
+        )
     }
-
-    componentDidMount(){
-        const url = "https://api.myjson.com/bins/w0alo";
-        fetch(url,{ 
-        method: "GET"
-    }).then(res => res.json())          // convert to plain text
-    .then(posts => {
-        console.log(posts) 
-        this.setState({posts: posts     // set state
-        },
-        function(){
-            console.log(this.state)
-        })   
-        })
-      
-    }
-
-
-  render() {
-      const columns = [
-      
-          {
-            Header: "POS",
-            id: "row",
-            maxWidth: 50,
-            filterable: false,
-            Cell: (row) => {
-              return <div>{row.index+1}</div>;
-            }
-          },
-          {
-            Header: "Team Name",
-            accessor: "name",
-            maxWidth: 200
-          },
-          {
-            Header: "ID",
-            accessor: "id",
-            maxWidth: 50
-
-          },
-    
-        ]
-      return(
-        <Jumbotron>
-             
-                <ReactTable
-                    columns={columns}
-                    data={this.state.posts}
-                >
-                </ReactTable>
-
-        </Jumbotron>
-      )
-  }
 }
 
 export default LeagueTable;
