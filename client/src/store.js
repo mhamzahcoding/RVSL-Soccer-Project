98738491bc3cf6711968f7d016718ecd4d2b3935
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-24 18:57:39 -0500

    redux mofified

--- client/src/store.js
+++ client/src/store.js
@@ -6,14 +6,22 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+    const enhancer = composeEnhancers(
+      applyMiddleware(...middleware),
+      // other store enhancers if any
+    );
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(
-    applyMiddleware(...middleware)
-  )
+  enhancer
 );
 
 export default store;
