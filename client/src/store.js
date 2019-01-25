38187c4c822c9829079d6a2909cd5855f67af7c0
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-24 19:29:07 -0500

    trying to fix redux

--- client/src/store.js
+++ client/src/store.js
@@ -6,20 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers =
-    typeof window === 'object' &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-    const enhancer = composeEnhancers(
-      applyMiddleware(...middleware),
-      // other store enhancers if any
-    );
-
 const store = createStore(
   rootReducer,
   initialState,
-  enhancer
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
+  )
 );
 
 export default store;
