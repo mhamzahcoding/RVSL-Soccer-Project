7c820d6c38319215824b77b8b3f930c5ece7e9e6
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-26 12:25:27 -0500

    table styling

--- client/src/components/rosterTable/RosterTable.js
+++ client/src/components/rosterTable/RosterTable.js
@@ -14,7 +14,7 @@ class RosterTable extends Component {
           <ErrorBoundary>
           <Jumbotron>
             <div>
-                <p style={{ fontWeight: "bold"}}>
+                <p style={{ fontWeight: "bold", textAlign: "center"}}>
                     Team Roster
                 </p>
             </div>
