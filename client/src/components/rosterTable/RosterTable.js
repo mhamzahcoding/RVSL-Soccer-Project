983f6cd086229cebad785cc902a275573b0e5d1d
mhamzahhanif <mhamzah1993@gmail.com>
2019-01-26 10:12:18 -0500

    roster

--- client/src/components/rosterTable/RosterTable.js
+++ client/src/components/rosterTable/RosterTable.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 //import ReactTable from 'react-table'
 import 'react-table/react-table.css'
 import { Jumbotron } from 'react-bootstrap'
-import Roster from './RosterTable';
+import Roster from './jax';
 import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import ErrorBoundary from '../../components/errorboundaries';
 
